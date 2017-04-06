0bde20d0a490af4da9ff7ec31bc73d00ba475ca5
Endre Kovacs <ekovacs@b2international.com>
2017-04-06 17:33:47 +0200

    adding the store with browser history sync support

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -7,15 +7,21 @@ import Main from './components/Main';
 import PhotoGrid from './components/PhotoGrid';
 import Single from './components/Single';
 
-import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {Router, Route, IndexRoute} from 'react-router';
+
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+
 
 const router = (
-  <Router history={browserHistory}>
+  <Provider store={store}>
+  <Router history={history}>
     <Route path="/" component={Main}>
       <IndexRoute component={PhotoGrid}></IndexRoute>
       <Route path="/view/:postId" component={Single}></Route>
     </Route>
   </Router>
+  </Provider>
 );
 
 render(router, document.getElementById('root'));
