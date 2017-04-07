27e3d7752b78e12af439042ee28f381672850d1e
Endre Kovacs <ekovacs@b2international.com>
2017-04-07 17:55:44 +0200

    implementing post action state reducer
    slicing and dicing state with the help of spread and object spread operator

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,7 +3,25 @@
 
 function posts(state = [], action) {
   console.log(state, action);
-  return state;
+  switch(action.type) {
+    case 'INCREMENT_LIKES':
+      const i = action.index;
+      return [
+        
+        // speading the first part of the state array, before the modified one
+        ...state.slice(0, i), 
+
+      { 
+        /* creating a new object: containing the: spreading the unmodified part of the object, and the likes incremented */
+        ...state[i], likes: state[i].likes + 1 
+      },
+        // speading the last part of the state array, after the modified one
+        ...state.slice(i + 1)
+      ]
+    default:
+      return state;
+  }
+  
 };
 
 export default posts;
