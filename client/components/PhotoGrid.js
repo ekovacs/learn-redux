11e583d04c22c49dbf0bbd2b66999ff15048f188
Endre Kovacs <ekovacs@b2international.com>
2017-04-07 17:53:54 +0200

    hooking in Photo react comp. to Photo grid
    ... passing down the connected props, and post related key values

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,11 +1,17 @@
 import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
+import Photo from './Photo';
 
 class PhotoGrid extends Component {
 
   render() {
     return (
-      <div className="photo-grid">I am the PhotoGrid</div>
+      <div className="photo-grid"> 
+            {
+                this.props.posts.map( (post, index) => 
+                    <Photo {...this.props} key={index} index={index} post={post} />) 
+            }
+      </div>
       )
   }
 
