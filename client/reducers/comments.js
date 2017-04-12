9b16d12e10fbcc64c14708f9d41458619c190592
Endre Kovacs <ekovacs@b2international.com>
2017-04-12 16:03:57 +0200

    added comments state reducer
    with state reducer composition

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -3,7 +3,38 @@
 
 function comments(state = [], action) {
   console.log(state, action);
+
+  if (typeof action.postId !== 'undefined') {
+    return {
+      // take the current state
+      ...state,
+      // override this post with a new one
+      [action.postId]: postComment(state[action.postId], action)
+    }
+  }
+
   return state;
 };
+
+function postComment(state = [], action) {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      return [
+        ...state, {
+          user: action.author, 
+          text: action.comment
+        }
+      ]
+      state;
+    case 'REMOVE_COMMENT':
+      return [
+        ...state.slice(0, action.index),
+        ...state.slice(action.index + 1)
+      ];
+    default: 
+      return state;
+  }
+  return state;
+}
 
 export default comments;
